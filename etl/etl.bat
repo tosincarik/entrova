4700559006a121870e5912631140554a097be4a7
Oluwatosin Oyeladun <toyeladun@gmail.com>
2025-10-02 14:51:26 +0100

    log files available for use

---
+++ etl/etl.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Navigate to repo folder
+cd C:\Users\Admin\projects\entrova\etl
+
+REM Run the ETL with Python 3.13
+C:\Users\Admin\AppData\Local\Programs\Python\Python313\python.exe etl_pipeline.py
